d20b09d5abd081c85a05baebbab7da059faa064b
ChrisALoor <crishernan_1610@hotmail.com>
2024-02-12 13:05:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chrisaloor.github.io/maps-main/'
+  base: 'https://chrisaloor.github.io/TrafficSim-Vis/'
 })
